f9f2b782bdae208b37af35f3461bac4472a02ed7
Guo Xiaosong <mztester@gmail.com>
2015-05-13 15:07:26 +0800

    add monolog/monolog, illuminate/database

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 define('APPLICATION_PATH', dirname(__DIR__));
 define('APPLICATION_ENV', 'develop');
 
+require APPLICATION_PATH . '/vendor/autoload.php';
+
 $application = new \Yaf\Application( APPLICATION_PATH . "/config/application.ini", APPLICATION_ENV);
 
 $application->bootstrap()->run();
