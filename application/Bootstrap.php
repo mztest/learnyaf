ba419e544b0c51619669fd7889a98bd1aedc4344
Guo Xiaosong <mztester@gmail.com>
2015-05-14 10:39:11 +0800

    1. config illuminate/database

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -7,17 +7,35 @@
  * 这些方法, 都接受一个参数:Yaf_Dispatcher $dispatcher
  * 调用的次序, 和申明的次序相同
  */
+use Illuminate\Database\Capsule\Manager as Capsule;
 class Bootstrap extends \Yaf\Bootstrap_Abstract {
     public function _initConfig() {
         // 把配置保存起来
-        $arrConfig = \Yaf\Application::app()->getConfig();
-        \Yaf\Registry::set('config', $arrConfig);
+        $config = \Yaf\Application::app()->getConfig();
+        \Yaf\Registry::set('config', $config);
     }
     public function _initPlugin(\Yaf\Dispatcher $dispatcher) {
         // 注册一个插件
     }
     public function _initRoute(\Yaf\Dispatcher $dispatcher) {
         // 在这里注册自己的路由协议,默认使用简单路由
+    }
+    
+    public function _initDabase(Yaf\Dispatcher $dispatcher) {
+        $config = Yaf\Registry::get('config');
+        $database = $config->get('database')->toArray();
+        
+        $capsule = new Capsule;
+        
+        $capsule->addConnection($database);
+        
+        // Make this Capsule instance available globally via static methods... (optional)
+        // $capsule->setAsGlobal();
+        
+        // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+        $capsule->bootEloquent();
+        
+        Yaf\Registry::set('Capsule', $capsule->getConnection());
     }
     /**
      *
